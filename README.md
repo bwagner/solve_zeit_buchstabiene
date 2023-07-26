c4461b92f04be86fb59cdd289f2497c0d4711b57
Bernhard Wagner <gitcommit@nosuch.biz>
2023-07-26 19:04:46 +0200

    ADDS: reqs, script

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# solve_zeit_buchstabiene
+# Find Some Words For Buchstabiene Riddle Of ["Die Zeit"](https://zeit.de)
+
+Simple Python script that attempts all words in a file for a given map.
+
+See [Buchstabiene](https://spiele.zeit.de/buchstabiene/)
+
+## Prerequisites
+- Download a word list which consists of one German word per line, e.g. [this file](https://gist.github.com/MarvinJWendt/2f4f4154b8ae218600eb091a5706b5f4).
+- The Script assumes the file to be installed here:
+  `~/Downloads/wordlist-german.txt`
+- install dependencies, e.g. `pip install -r requirements.txt`
+- Before running the script, open the riddle in a web browser, then click on
+	the terminal from where you'll run the script.  The script will attempt to
+	change the focus to the web browser by simulating typing of Cmd-Tab for macOS,
+	Ctrl-Tab for Windows
+
+## Map
+
+The script needs to know what the current map is.
+
+The map is defined by the "flower": its center (seeds) and 8 Petals, hence 9 characters.
+
+Ascii-Representation of the riddle of 26.04.2023:
+
+```
+
+            P   O
+           T     W
+              T
+           A     U
+            R   H
+
+```
+We map these characters as follows:
+
+```
+            8   1
+           7     2
+              0
+           6     3
+            5   4
+
+```
+
+Hence the script would be called like this:
+
+`./solve_zeit_buchstabiene.py towuhratp`
+
---
+++ solve_zeit_buchstabiene.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+
+import sys
+from pathlib import Path
+
+import pyautogui
+import pytest
+
+# So far only used on
+# macOS 13.4.1 (Ventura).
+# Python 3.11.4
+
+# See it in action: https://youtu.be/O0rWH5hdgx0
+
+# wordlist from here:
+# https://gist.github.com/MarvinJWendt/2f4f4154b8ae218600eb091a5706b5f4
+word_list = Path("~/Downloads/wordlist-german.txt").expanduser()
+
+
+def switch_apps_1():
+    """
+    Supposed to switch between applications by pressing Cmd-Tab.
+    Does not work on the tested system macOS 13.4.1 Ventura.
+    """
+    pyautogui.hotkey("command", "tab")
+
+
+def switch_apps_2():
+    """
+    Switches between applications by pressing Cmd-Tab.
+    Works on the tested system macOS 13.4.1 Ventura.
+    """
+    with pyautogui.hold("command"):
+        pyautogui.press("tab")
+
+
+def switch_apps_3():
+    """
+    Switches between applications by pressing Cmd-Tab.
+    Works on the tested system macOS 13.4.1 Ventura.
+    """
+    pyautogui.keyDown("command")
+    pyautogui.press("tab")
+    pyautogui.keyUp("command")
+
+
+def get_petals(mapping: str, word: str):
+    """
+    Translates word characters into flower petals as a list with our numbering.
+    """
+    mapping: list[str] = list(mapping)
+    petals = [None] * len(word)
+    i: int
+    ch: str
+    for i, ch in enumerate(word):
+        idx: int = mapping.index(ch)
+        mapping[idx] = "-"  # mark char no longer available
+        petals[i] = idx
+    return petals
+
+
+def word_ok(mapping: str, word: str):
+    """
+    Indicates whether word is representable with given mapping.
+    """
+    # the character in the center of the flower (seed) is mandatory.
+    if mapping[0] not in word:
+        return False
+
+    # minimum word length: 3
+    if len(word) < 3:
+        return False
+
+    # every petal of the flower may be used max once.
+    maplist = list(mapping)
+    try:
+        for ch in word:
+            maplist.remove(ch)  # removes 1st occurrence of char in list
+        return True
+    except ValueError:
+        return False
+
+
+def type_word(word):
+    """
+    Types the given word and concludes with Return.
+    Note: On macOS Ventura 13.4.1 pyautogui.press does not work for Umlauts.
+    """
+    for ch in word:
+        pyautogui.press(ch)
+    pyautogui.press("return")
+
+
+def handle_file(mapping, word_list_file):
+    """
+    Tries all words in the given file for the given mapping.
+    """
+    with open(word_list_file) as f:
+        for word in f:
+            word = word[:-1].lower()  # cut off newline
+            print(f"trying {word}: ", end="")
+            if word_ok(mapping, word):
+                print("candidate!")
+                try:
+                    type_word(word)
+                except Exception:
+                    print("error occurred. continuing...")
+            else:
+                print("nope.")
+
+
+## Tests #######################################################################
+
+test_input_verify = [
+    ("aemlbgria", "abla", True),
+    ("aemlbgria", "ablaa", False),
+    ("aemlbgria", "ablal", False),
+    ("aemlbgria", "gabel", True),
+    ("aemlbgria", "griaemlba", True),
+    ("aemlbgria", "griamlbae", True),
+    ("aemlbgria", "griaemlbae", False),
+    ("uitcphsts", "usts", True),
+]
+
+
+@pytest.mark.parametrize("mapping,word,ok_exp", test_input_verify)
+def test_verify_word(mapping, word, ok_exp):
+    assert word_ok(mapping, word) == ok_exp
+
+
+test_input_petals = [
+    ("aemlbgria", "abla", [0, 4, 3, 8]),
+    ("aemlbgria", "gabel", [5, 0, 4, 1, 3]),
+    ("aemlbgria", "griaemlba", [5, 6, 7, 0, 1, 2, 3, 4, 8]),
+    ("aemlbgria", "griamlbae", [5, 6, 7, 0, 2, 3, 4, 8, 1]),
+]
+
+
+@pytest.mark.parametrize("mapping,word,exp", test_input_petals)
+def test_petals(mapping, word, exp):
+    assert get_petals(mapping, word) == exp
+
+
+## Main ########################################################################
+
+
+def main():
+    if len(sys.argv) == 11:
+        mapping = "".join(sys.argv[1:11]).lower()
+        word = sys.argv[10].lower()
+    elif len(sys.argv) == 3:
+        mapping = sys.argv[1].lower()
+        word = sys.argv[2].lower()
+    elif len(sys.argv) == 2:
+        mapping = sys.argv[1].lower()
+        switch_apps_2()
+        handle_file(mapping, word_list)
+        return
+    else:
+        print(
+            "expected either 9 chars and 1 word "
+            "or 1 mapping "
+            f"or 2 words but got {len(sys.argv) - 1}."
+        )
+        return
+    if word_ok(mapping, word):
+        switch_apps_2()
+        click_coords = type_word(word)
+        print(click_coords)
+
+    else:
+        print(f"word {word} is not representable with {mapping} or is too short.")
+
+
+if __name__ == "__main__":
+    main()
